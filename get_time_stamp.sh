769ff258e793943a35dad821d2bb3788003728a7
Richard Kavanagh <r_kav_0000@hotmail.com>
2016-10-31 13:39:46 +0000

    Adding improvements to usability
    Adding a script that provides the start and end times of the data to
    collect and ensuring the output file is csv.

---
+++ get_time_stamp.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+date +%s
+date +%s >> timestamp.log
--- zabbix_data_collector.sh
+++ zabbix_data_collector.sh
@@ -13,7 +13,7 @@ if [ "$#" -ne 4 ]; then
     echo "Usage: zabbix_data_collector.sh hostname item_name start_time end_time"
 	echo "e.g.  ./zabbix_data_collector.sh testnode1 power 1439337600 1439373722"
 	if [ "$#" -eq 1 ]; then
-		mysql -h $hostname -u $username -p${password} -D $database --column_names --execute="SELECT it.itemid, it.name, it.key_ from hosts, items it WHERE hosts.hostid = it.hostid and hosts.host = '$host'" > "${host}_items_list.csv"
+		mysql -h $hostname -u $username -p${password} -D $database --column_names --execute="SELECT it.itemid, it.name, it.key_ from hosts, items it WHERE hosts.hostid = it.hostid and hosts.host = '$host'" | sed "s/'/\'/;s/\t/\",\"/g;s/^/\"/;s/$/\"/;s/\n//g" > "${host}_items_list.csv"
 	fi
 	exit 1
 fi
@@ -25,7 +25,7 @@ echo "End Time = " $end
 
 #doubles/ floats
 
-mysql -h $hostname -u $username -p${password} -D $database --column_names --execute="SELECT h.itemid, h.clock, i.name, i.key_, h.value FROM items i, history h WHERE h.itemid = i.itemid AND h.itemid = (select it.itemid from hosts, items it WHERE hosts.hostid = it.hostid and hosts.host = '$host' AND it.key_ = '$item') AND h.clock >= $start AND h.clock <= $end order by h.clock;" > "${host}_${item}_${start}_${end}.csv"
+mysql -h $hostname -u $username -p${password} -D $database --column_names --execute="SELECT h.itemid, h.clock, i.name, i.key_, h.value FROM items i, history h WHERE h.itemid = i.itemid AND h.itemid = (select it.itemid from hosts, items it WHERE hosts.hostid = it.hostid and hosts.host = '$host' AND it.key_ = '$item') AND h.clock >= $start AND h.clock <= $end order by h.clock;" | sed "s/'/\'/;s/\t/\",\"/g;s/^/\"/;s/$/\"/;s/\n//g" > "${host}_${item}_${start}_${end}.csv"
 
 if [[ -s "${host}_${item}_${start}_${end}.csv" ]] ; then
 	echo "The item found was of type double."
@@ -34,7 +34,7 @@ fi ;
 
 #integers
 
-mysql -h $hostname -u $username -p${password} -D $database --column_names --execute="SELECT h.itemid, h.clock, i.name, i.key_, h.value FROM items i, history_uint h WHERE h.itemid = i.itemid AND h.itemid = (select it.itemid from hosts, items it WHERE hosts.hostid = it.hostid and hosts.host = '$host' AND it.key_ = '$item') AND h.clock >= $start AND h.clock <= $end order by h.clock;" > "${host}_${item}_${start}_${end}.csv"
+mysql -h $hostname -u $username -p${password} -D $database --column_names --execute="SELECT h.itemid, h.clock, i.name, i.key_, h.value FROM items i, history_uint h WHERE h.itemid = i.itemid AND h.itemid = (select it.itemid from hosts, items it WHERE hosts.hostid = it.hostid and hosts.host = '$host' AND it.key_ = '$item') AND h.clock >= $start AND h.clock <= $end order by h.clock;" | sed "s/'/\'/;s/\t/\",\"/g;s/^/\"/;s/$/\"/;s/\n//g" > "${host}_${item}_${start}_${end}.csv"
 
 if [[ -s "${host}_${item}_${start}_${end}.csv" ]] ; then
 	echo "The item found was of type integer."
@@ -43,7 +43,7 @@ fi ;
 
 #Strings
 
-mysql -h $hostname -u $username -p${password} -D $database --column_names --execute="SELECT h.itemid, h.clock, i.name, i.key_, h.value FROM items i, history_str h WHERE h.itemid = i.itemid AND h.itemid = (select it.itemid from hosts, items it WHERE hosts.hostid = it.hostid and hosts.host = '$host' AND it.key_ = '$item') AND h.clock >= $start AND h.clock <= $end order by h.clock;" > "${host}_${item}_${start}_${end}.csv"
+mysql -h $hostname -u $username -p${password} -D $database --column_names --execute="SELECT h.itemid, h.clock, i.name, i.key_, h.value FROM items i, history_str h WHERE h.itemid = i.itemid AND h.itemid = (select it.itemid from hosts, items it WHERE hosts.hostid = it.hostid and hosts.host = '$host' AND it.key_ = '$item') AND h.clock >= $start AND h.clock <= $end order by h.clock;" | sed "s/'/\'/;s/\t/\",\"/g;s/^/\"/;s/$/\"/;s/\n//g" > "${host}_${item}_${start}_${end}.csv"
 
 if [[ -s "${host}_${item}_${start}_${end}.csv" ]] ; then
 	echo "The item found was of type string."
@@ -52,7 +52,7 @@ fi ;
 
 #text
 
-mysql -h $hostname -u $username -p${password} -D $database --column_names --execute="SELECT h.itemid, h.clock, i.name, i.key_, h.value FROM items i, history_text h WHERE h.itemid = i.itemid AND h.itemid = (select it.itemid from hosts, items it WHERE hosts.hostid = it.hostid and hosts.host = '$host' AND it.key_ = '$item') AND h.clock >= $start AND h.clock <= $end order by h.clock;" > "${host}_${item}_${start}_${end}.csv"
+mysql -h $hostname -u $username -p${password} -D $database --column_names --execute="SELECT h.itemid, h.clock, i.name, i.key_, h.value FROM items i, history_text h WHERE h.itemid = i.itemid AND h.itemid = (select it.itemid from hosts, items it WHERE hosts.hostid = it.hostid and hosts.host = '$host' AND it.key_ = '$item') AND h.clock >= $start AND h.clock <= $end order by h.clock;" | sed "s/'/\'/;s/\t/\",\"/g;s/^/\"/;s/$/\"/;s/\n//g" > "${host}_${item}_${start}_${end}.csv"
 
 if [[ -s "${host}_${item}_${start}_${end}.csv" ]] ; then
 	echo "The item found was of type text."
