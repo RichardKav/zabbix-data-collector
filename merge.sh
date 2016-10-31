54d8af22a06357daba8399dd0b91c56f3f5f33cf
Richard Kavanagh <r_kav_0000@hotmail.com>
2016-10-31 22:15:20 +0000

    Simplifying the script.

--- merge.sh
+++ merge.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-file="all.csv"
-if [ -f $file ] ; then
-    rm $file
-fi
-awk 'FNR>1 || NR==1 {split(FILENAME,host,"_"); print $0, ",", (NR==1)? "host" : host[1]}' *.csv >> all.csv
+awk 'FNR>1 || NR==1 {split(FILENAME,host,"_"); print $0, ",", (NR==1)? "host" : host[1]}' *.csv > all.csv
